5ff7605da8e155c2764ea50b8eac2c0b3ff70f72
joecode <joseph.marchant@satavia.com>
2018-09-02 00:27:46 +0100

    Removed Board.finished in favour of return values, and cleaned up variable names

--- board.py
+++ board.py
@@ -5,52 +5,34 @@ class Board(object):
 
     def __init__(self):
         self.state = [0]*9
-        self.finished = False
         self.moves_count = 0
 
-    def check_finished(self, player):
+    def check_finished(self, p):
         """Quick Function to check if game is finished (3 in a row or draw)
         Args:
-            player (int): Returns player: 1 = Player 1, 2 = Player 2
+            p (int): Last Player: 1 = Player 1, 2 = Player 2
         Returns:
             Winner (int): 0 = Game Unfinished, 1 = Player 1, 2 = Player 2, 3 = Draw
         """
-
         if self.moves_count > 4:
+            s = self.state
             # Game can't have finished in under 5 moves
-            if self.state[4] == player:
+            if s[4] == p:  # i.e, if player p has played in position 4 in Board.state
                 # Half 3 in a rows include middle square
-                if self.state[0] == player and self.state[8] == player:
-                    self.finished = True
-                    return player
-                elif self.state[2] == player and self.state[6] == player:
-                    self.finished = True
-                    return player
-                elif self.state[1] == player and self.state[7] == player:
-                    self.finished = True
-                    return player
-                elif self.state[3] == player and self.state[5] == player:
-                    self.finished = True
-                    return player
+                if (s[0] == p and s[8] == p) or (s[2] == p and s[6] == p):
+                    return p
+                elif (s[1] == p and s[7] == p) or (s[3] == p and s[5] == p):
+                    return p
             # No 3 in a row through middle
-            if self.state[0] == player:
-                if self.state[6] == player and self.state[3] == player:
-                    self.finished = True
-                    return player
-                elif self.state[1] == player and self.state[2] == player:
-                    self.finished = True
-                    return player
-            elif self.state[8] == player:
-                if self.state[6] == player and self.state[7] == player:
-                    self.finished = True
-                    return player
-                elif self.state[2] == player and self.state[5] == player:
-                    self.finished = True
-                    return player
+            if s[0] == p:
+                if (s[6] == p and s[3] == p) or (s[1] == p and s[2] == p):
+                    return p
+            elif s[8] == p:
+                if (s[6] == p and s[7] == p) or (s[2] == p and s[5] == p):
+                    return p
             # No 3 in a row
             elif self.moves_count == 8:
                 # Board must be full
-                self.finished = True
                 return 3
         return 0
 
