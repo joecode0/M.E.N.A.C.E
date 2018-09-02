48de178c5175984491ad209da84bad4f177b28bf
joecode <joseph.marchant@satavia.com>
2018-09-02 18:14:08 +0100

    Super fast permutations function that removes duplicates, used to quickly generate all possible boards where a specific player plays next

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
-import itertools
-
-
-def perms(x):
-    return list(itertools.permutations(x, r=9))
+def perms(nums):
+    perms = [[]]
+    for n in nums:
+        new_perm = []
+        for perm in perms:
+            for i in range(len(perm) + 1):
+                new_perm.append(perm[:i] + [n] + perm[i:])
+                # handle duplication
+                if i < len(perm) and perm[i] == n:
+                    break
+        perms = new_perm
+    return perms
